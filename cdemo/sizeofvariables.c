ae4aa36fa5824893f26b72d40c53ec4bb3777935
paizc0618 <imctp0205@gmail.com>
2019-05-23 15:19:13 +0000

    finixed sizeofvariables

--- cdemo/sizeofvariables.c
+++ cdemo/sizeofvariables.c
@@ -3,8 +3,14 @@
 int main()
 {
   int a = 545;
+  char b = 'b';
+  float c = 1.5;
+  double d = 2.5;
 
   // print value and size of an int variable
-  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("int a value: %d and size: %lu bytes\n", a, sizeof(a));
+  printf("int a value: %c and size: %lu bytes\n", b, sizeof(b));
+  printf("int a value: %f and size: %lu bytes\n", c, sizeof(c));
+  printf("int a value: %lf and size: %lu bytes\n", d, sizeof(d));
 }
 
