d75e65fb59ea24ffcb71c7625576e43f9404ec3b
paizc0618 <imctp0205@gmail.com>
2019-05-29 15:50:15 +0000

    fixed student

--- cdemo/student.c
+++ cdemo/student.c
@@ -26,34 +26,6 @@ int main()
   strcpy(student1.first, "Collin");
   strcpy(student1.last, "Paiz");
   student1.age = 18;
-  student1.studentid = 840618;#include <stdio.h>
-#include <string.h>
-
-struct Student
-{
-  char first[50];
-  char last[50];
-  int age;
-  int studentid;
-};
-
-void printStudent(struct Student* student)
-{
-  printf("Name: %s %s\n", student->first, student->last);
-  printf("Age: %d\n", student->age);
-  printf("StudentID: %d\n", student->studentid);
-}
-
-int main()
-{
-
-  char input[256];
-  struct Student student1;
-  struct Student student2;
-
-  strcpy(student1.first, "Collin");
-  strcpy(student1.last, "Paiz");
-  student1.age = 18;
   student1.studentid = 840618;
 
   printf("What is your first name?\n");
