234decc129596966811d6ef749aad8f607a7f27a
paizc0618 <imctp0205@gmail.com>
2019-05-29 15:04:50 +0000

    finished strings

--- cdemo/strings.c
+++ cdemo/strings.c
@@ -3,8 +3,30 @@
 
 int main()
 {
-
-  
-  char str2[] = "abcdefghijklmnopqrstuvwxyz";
-
+  int i;
+  int x;
+  char alph1[26];
+  char alph2[26] = "abcdefghijklmnopqrstuvwxyz";
+  for (i = 0; i <= 25; i++)
+  {
+    alph1[i] = 97 + i;
+  }
+  for (x = 0; x <= 25; x++)
+  {
+    alph2[x] = 65 + x;
+  }
+  printf("%s\n", alph1);
+  printf("%s\n", alph2);
+  if (strcmp(alph1, alph2) == 0)
+  {
+    printf("Strings are the same.\n");
+  }
+  else
+  {
+    printf("The strings are not the same.\n");
+  }
+  char alph3[52];
+  strcpy(alph3, alph1);
+  strcat(alph3, alph2);
+  printf("%s, %s, %s \n", alph1, alph2, alph3);
 }
